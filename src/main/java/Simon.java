50ddfb6dd28890558fc270512e68e2e2e9675f79
IsaiahToh <e1337592@u.nus.edu>
2025-08-20 01:09:10 +0800

    Rename main class

---
+++ src/main/java/Simon.java
@@ -0,0 +1,12 @@
+public class Simon {
+    public static void main(String[] args) {
+        String logo = "   _____ _                     \n"
+                + "  / ___/(_)___ ___  ____  ____ \n"
+                + "  \\__ \\/ / __ `__ \\/ __ \\/ __ \\\n"
+                + " ___/ / / / / / / / /_/ / / / /\n"
+                + "/____/_/_/ /_/ /_/\\____/_/ /_/ \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
+
+
