26c93a830d5da3ccbbab4b19a0bdad484e450647
IsaiahToh <e1337592@u.nus.edu>
2025-08-20 15:04:28 +0800

    Add Todo, Deadline, Event

--- src/main/java/Simon.java
+++ src/main/java/Simon.java
@@ -6,11 +6,13 @@ public class Simon {
         Task[] tasks = new Task[100];
         int taskCount = 0;
 
-        String logo = "   _____ _                     \n"
-                + "  / ___/(_)___ ___  ____  ____ \n"
-                + "  \\__ \\/ / __ `__ \\/ __ \\/ __ \\\n"
-                + " ___/ / / / / / / / /_/ / / / /\n"
-                + "/____/_/_/ /_/ /_/\\____/_/ /_/ \n";
+        String logo = """
+           _____ _                     
+          / ___/(_)___ ___  ____  ____ 
+          \\__ \\/ / __ `__ \\/ __ \\/ __ \\
+         ___/ / / / / / / / /_/ / / / /
+        /____/_/_/ /_/ /_/\\____/_/ /_/ 
+        """;
         System.out.println(
             "____________________________________________________________\n" 
             + logo 
@@ -86,15 +88,50 @@ public class Simon {
                         ____________________________________________________________
                         """);
                 }
-            } else {
-                tasks[taskCount] = new Task(input);
+            } else if (input.startsWith("todo ")) {
+                String description = input.substring(5);
+                tasks[taskCount] = new Todo(description);
                 taskCount++;
                 System.out.println(
                     "____________________________________________________________\n" 
-                    + " added: "
-                    + input
-                    + "\n____________________________________________________________"
+                    + " Got it. I've added this task:\n"
+                    + "   " + tasks[taskCount - 1]
+                    + "\n Now you have " + taskCount + " tasks in the list.\n"
+                    + "____________________________________________________________"
                 );
+            } else if (input.startsWith("deadline")) {
+                String[] parts = input.substring(9).split(" /by ", 2);
+                String description = parts[0];
+                String by = parts[1];
+                tasks[taskCount] = new Deadline(description, by);
+                taskCount++;
+                System.out.println(
+                    "____________________________________________________________\n" 
+                    + " Got it. I've added this task:\n"
+                    + "   " + tasks[taskCount - 1]
+                    + "\n Now you have " + taskCount + " tasks in the list.\n"
+                    + "____________________________________________________________"
+                );
+            } else if (input.startsWith("event")) {
+                String[] parts = input.substring(6).split(" /from | /to ", 3);
+                String description = parts[0];
+                String start = parts[1];
+                String end = parts[2];
+                tasks[taskCount] = new Event(description, start, end);
+                taskCount++;
+                System.out.println(
+                    "____________________________________________________________\n" 
+                    + " Got it. I've added this task:\n"
+                    + "   " + tasks[taskCount - 1]
+                    + "\n Now you have " + taskCount + " tasks in the list.\n"
+                    + "____________________________________________________________"
+                );
+            } else {
+                System.out.println("""
+                    ____________________________________________________________
+                     Sorry, not trained for that. Use todo, deadline, and event to add a task :)
+                    ____________________________________________________________
+                    """);
             }
         }
         scanner.close();
